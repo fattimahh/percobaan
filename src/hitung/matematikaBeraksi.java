f7eb9d4f0e69aaaaf223aff4e405f62fde1f9eaa
fatimahproject <fatimah140903@gmail.com>
2023-06-19 18:56:12 +0700

    membuat class matematikaBeraksi

--- src/hitung/matematikaBeraksi.java
+++ src/hitung/matematikaBeraksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author asus
- */
 public class matematikaBeraksi {
+    public static void main(String[] args) {
+    //membuat objek
+    matematika imah = new matematika(8,8);
     
+    System.out.println("Hasil penjumlahan: "+imah.setPenjumlahan());
+    System.out.println("Hasil pengurangan: "+imah.setPengurangan());
+    System.out.println("Hasil perkalian: "+imah.setPerkalian());
+    System.out.println("Hasil pembagian: "+imah.setPembagian());
+    
+    
+    }
 }
+
